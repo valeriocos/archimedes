46c8b27c4ffa20d0595b3d01c2402d533a8df385
Valerio Cosentino <valcos@bitergia.com>
2019-02-13 16:48:33 +0100

    [archimedes] Move `load_json` to utils file
    This code converts the `load_json` method in the manager class to a
    function and moves it to the utils file.

---
+++ archimedes/utils.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2014-2018 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Authors:
+#   Valerio Cosentino <valcos@bitergia.com>
+#
+
+import json
+
+
+def load_json(file_path):
+    """Load JSON content from file.
+
+    :param file_path: the path of a JSON file
+
+    :returns: JSON content
+    """
+    with open(file_path, 'r') as f:
+        content = f.read()
+
+    json_content = json.loads(content)
+    return json_content
